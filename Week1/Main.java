d84d374182e1431fb5c6246ae5485a4486536fa2
TamasGaborrr <73156758+TamasGaborrr@users.noreply.github.com>
2026-02-09 10:54:30 +0100

    Add files via upload

---
+++ Week1/Main.java
@@ -0,0 +1,6 @@
+void main() {
+    System.out.println("Hello World!");
+    Bottle waterBottle = new Bottle("green", 1.5);
+    System.out.println(waterBottle.color +" "+waterBottle.capacity+"l");
+
+}
