8e23a1cc520f4cbd19f17ecf22a0e8ac611059fd
TamasGaborrr <73156758+TamasGaborrr@users.noreply.github.com>
2026-02-09 10:52:52 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-void main() {
-    System.out.println("Hello World!");
-    Bottle waterBottle = new Bottle("green", 1.5);
-    System.out.println(waterBottle.color +" "+waterBottle.capacity+"l");
-
-}
